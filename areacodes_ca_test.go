923e4e66151b32137533f993acf5c87cf05ef6a3
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-07-12 19:58:33 -0700

    areacodes

---
+++ areacodes_ca_test.go
@@ -0,0 +1,59 @@
+package e164
+
+var (
+	areaCodesCA = []string{
+		"204", // Manitoba
+		"226", // London, ON
+		"236", // Vancouver, BC
+		"249", // Sudbury, ON
+		"250", // Kelowna, BC
+		"263", // Montreal, QC
+		"289", // Hamilton, ON
+		"306", // Saskatchewan
+		"343", // Ottawa, ON
+		"354", // Granby, QC
+		"365", // Hamilton, ON
+		"367", // Quebec, QC
+		"368", // Calgary, AB
+		"382", // London, ON
+		"403", // Calgary, AB
+		"416", // Toronto, ON
+		"418", // Quebec, QC
+		"428", // New Brunswick
+		"431", // Manitoba
+		"437", // Toronto, ON
+		"438", // Montreal, QC
+		"450", // Granby, QC
+		"468", // Sherbrooke, QC
+		"474", // Saskatchewan
+		"506", // New Brunswick
+		"514", // Montreal, QC
+		"519", // London, ON
+		"548", // London, ON
+		"579", // Granby, QC
+		"581", // Quebec, QC
+		"584", // Manitoba
+		"587", // Calgary, AB
+		"604", // Vancouver, BC
+		"613", // Ottawa, ON
+		"639", // Saskatchewan
+		"647", // Toronto, ON
+		"672", // Vancouver, BC
+		"683", // Sudbury, ON
+		"705", // Sudbury, ON
+		"709", // Newfoundland/Labrador
+		"742", // Hamilton, ON
+		"753", // Ottawa, ON
+		"778", // Vancouver, BC
+		"780", // Edmonton, AB
+		"782", // Nova Scotia/PE Island
+		"807", // Kenora, ON
+		"819", // Sherbrooke, QC
+		"825", // Calgary, AB
+		"867", // Northern Canada
+		"873", // Sherbrooke, QC
+		"879", // Newfoundland/Labrador
+		"902", // Nova Scotia/PE Island
+		"905", // Hamilton, ON
+	}
+)
